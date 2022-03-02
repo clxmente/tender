75c668e3cbcc2f497e7f79fb5d16c7a9c3512034
clxmente <solorioeclem164@gmail.com>
2022-03-02 01:15:16 -0800

    [TN-9] questions page WIP with 2 sets of checkboxes

---
+++ components/Checkbox.jsx
@@ -0,0 +1,22 @@
+export default function Checkbox(props) {
+  return (
+    <fieldset className="space-y-5">
+      <div className="flex items-start rounded-md bg-indigo-600 py-3 px-3">
+        <div className="flex items-center h-5">
+          <input
+            id={props.label}
+            aria-describedby={props.label + "-description"}
+            name={props.label}
+            type="checkbox"
+            className="ring-indigo-500 h-4 w-4 text-gray-500 border-gray-300 rounded"
+          />
+        </div>
+        <div className="ml-3 text-sm">
+          <label htmlFor={props.label} className="font-bold text-white">
+            {props.label}
+          </label>
+        </div>
+      </div>
+    </fieldset>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="bg-gray-50">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
