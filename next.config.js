2f5dcfca49e846dd085919579b8febf1a6df3226
clxmente <solorioeclem164@gmail.com>
2022-03-21 14:05:04 -0700

    first recipe image/desc/title

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
   images: {
-    domains: ["images.unsplash.com", "loveincorporated.blob.core.windows.net"]
+    domains: [ "www.edamam.com", "images.unsplash.com", "loveincorporated.blob.core.windows.net"]
   }
 }
--- pages/swipe.js
+++ pages/swipe.js
@@ -1,39 +1,59 @@
 import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Swipe() {
   const [imgSrc, setImgSrc] = useState("/starting_dish.jpg");
   const [foodTitle, setFoodTitle] = useState("Food Title");
-  const [foodDesc, setFoodDesc] = useState("Lore ipsum dolor sit amet, consectetur adipiscing elit. Sed ac lorem sem. Mauris vehicula a.");
+  // const [foodDesc, setFoodDesc] = useState("Lore ipsum dolor sit amet, consectetur adipiscing elit. Sed ac lorem sem. Mauris vehicula a.");
+  const [ingList, setIngList] = useState([]);
+  const [foodList, setFoodList] = useState({});
+  const [itemNum, setItemNum] = useState(0);
 
   function handleDislike() {
+    // get a new recipe
     axios.get("https://random.imagecdn.app/v1/image?width=600&height=600&format=text").then(res => {
       setImgSrc(res.data);
     })
   }
 
   function handleLike() {
+    // get a new recipe & save recipe ID to json file
     axios.get("https://random.imagecdn.app/v1/image?width=600&height=600&format=text").then(res => {
       setImgSrc(res.data);
     })
   }
 
+  useEffect(async () => {
+    // query API for first batch of results
+    const response = await fetch(
+      `/api/getrecipes`
+    );
+    const data = await response.json();
+
+    setIngList(data[itemNum].recipe.ingredientLines); // get the ingredient list
+    setFoodTitle(data[itemNum].recipe.label); // get the recipe title
+    setImgSrc(data[itemNum].recipe.images.LARGE["url"]); // get image url
+    
+  }, [])
+
   return ( 
     <div className="flex justify-center">
       <div>
-        <div className="w-96 h-96 lg:w-[527px] lg:h-[424px] object-cover rounded-md overflow-hidden drop-shadow-md">
+        <div className="w-96 h-96 lg:w-[600px] lg:h-[600px] object-cover rounded-md overflow-hidden drop-shadow-md">
           <Image
             src={imgSrc}
             alt={"Food Image"}
-            width={527}
-            height={424}
+            width={600}
+            height={600}
             priority
           />
         </div>
         <h1 className="font-bold text-lg mt-10">{foodTitle}</h1>
-        <p className="text-lg text-[#848484] w-96 lg:w-[527px]">{foodDesc}</p>
+        <ul className="text-lg text-[#848484] w-96 lg:w-[600px] list-disc ml-5">
+          {ingList.map((line) => {return <li>{line}</li>})}
+        </ul>
         <div className="mt-5 grid grid-cols-2 gap-10">
           <button onClick={handleDislike} className="rounded-md drop-shadow-md bg-red-500 hover:bg-red-600 px-3 py-2 text-white font-semibold">Dislike</button>
           <button onClick={handleLike} className="rounded-md drop-shadow-md bg-green-500 hover:bg-green-600 px-3 py-2 text-white font-semibold">Like</button>
