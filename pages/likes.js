4e0d0a9de256b052033cbe989e7863921145e92e
matthewiversen <iversenmatt@yahoo.com>
2022-04-23 19:21:30 -0700

    TN-33 split recipes and restaurants

--- pages/likes.js
+++ pages/likes.js
@@ -1,65 +1,154 @@
+import { useState } from "react";
 import Head from "next/head";
 import Liked from "../components/Liked";
 import Link from "next/link";
+import RestaurantCard from "../components/RestaurantCard";
 
-export default function Likes({ LikedRecipes }) {
-  if (LikedRecipes.length == 0) {
-    return (
-      <div>
-        <div className="pt-[100px]">
-          <p className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl m-auto text-gray-900 text-left w-[70%]">
-            Looks like there are no likes here.
-          </p>
-          <p className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl m-auto text-gray-900 text-left w-[70%]">
-            Head back home to browse some food!
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <Link href={"/"} passHref>
-            <a className="w-[70%] mx-5">
-              <p className="mt-5 p-3 pb-4 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 drop-shadow-md text-center text-2xl">
-                I'm starving...
-              </p>
-            </a>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+export default function Likes({ LikedRecipes, LikedRestaurants }) {
+	const [viewState, setViewState] = useState("recipes");
 
-  const liked_cards = LikedRecipes.map((recipe) => {
-    return (
-      <Liked
-        key={recipe.recipeID}
-        label={recipe.title}
-        score={recipe.healthScore}
-        readyIn={recipe.readyIn}
-        button={"Recipe"}
-        url={recipe.url}
-        image={recipe.image}
-        servings={recipe.servings}
-        aggregateLikes={recipe.aggregateLikes}
-        summary={recipe.summary}
-      />
-    );
-  });
+	function changeToRecipes() {
+		setViewState("recipes");
+	}
+	function changeToRestaurants() {
+		setViewState("restaurants");
+	}
 
-  return (
-    <div>
-      <Head>
-        <title>Tender: Likes</title>
-      </Head>
-      <div className="mx-6 my-6 grid grid-cols-1 gap-3">{liked_cards}</div>
-    </div>
-  );
+	// no likes
+	if (LikedRecipes.length == 0 && LikedRestaurants.length == 0) {
+		return (
+			<div>
+				<div className="pt-[100px]">
+					<p className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl m-auto text-gray-900 text-left w-[70%]">
+						Looks like there are no likes here.
+					</p>
+					<p className="font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl m-auto text-gray-900 text-left w-[70%]">
+						Head back home to browse some food!
+					</p>
+				</div>
+				<div className="flex justify-center">
+					<Link href={"/"} passHref>
+						<a className="w-[70%] mx-5">
+							<p className="mt-5 p-3 pb-4 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 drop-shadow-md text-center text-2xl">
+								I&apos;m starving...
+							</p>
+						</a>
+					</Link>
+				</div>
+			</div>
+		);
+	} else {
+		// generate likes if there is at least one like
+		const liked_recipes = LikedRecipes.map((recipe) => {
+			return (
+				<Liked
+					key={recipe.recipeID}
+					label={recipe.title}
+					score={recipe.healthScore}
+					readyIn={recipe.readyIn}
+					button={"Recipe"}
+					url={recipe.url}
+					image={recipe.image}
+					servings={recipe.servings}
+					aggregateLikes={recipe.aggregateLikes}
+					summary={recipe.summary}
+				/>
+			);
+		});
+
+		console.log(liked_recipes);
+
+		const liked_restaurants = LikedRestaurants.map((restaurant) => {
+			return (
+				<RestaurantCard
+					key={restaurant.restaurantID}
+					name={restaurant.name}
+					description={restaurant.categories}
+					address1={restaurant.streetAddress}
+					address2={restaurant.secondaryAddress}
+					city={restaurant.city}
+					state={restaurant.state}
+					zip={restaurant.zip}
+					phone={restaurant.phone}
+					categories={restaurant.categories}
+				/>
+			);
+		});
+
+		// display cards
+		if (viewState == "recipes") {
+			return (
+				<div>
+					<Head>
+						<title>Tender: Recipe Likes</title>
+					</Head>
+					<div className="ml-6 mt-3">
+						<button
+							onClick={() => {
+								changeToRecipes();
+							}}
+							className="inline-flex items-center px-4 py-2 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 drop-shadow-md"
+						>
+							Recipes
+						</button>
+						<button
+							onClick={() => {
+								changeToRestaurants();
+							}}
+							className="inline-flex items-center ml-2 px-4 py-2 border border-gray-300 text-base font-semibold rounded-md text-indigo-600 bg-white hover:bg-gray-50 drop-shadow-md"
+						>
+							Restaurants
+						</button>
+					</div>
+					<div className="mx-6 my-6 grid grid-cols-1 gap-3">
+						{liked_recipes}
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<Head>
+						<title>Tender: Restaurant Likes</title>
+					</Head>
+					<div className="ml-6 mt-3">
+						<button
+							onClick={() => {
+								changeToRecipes();
+							}}
+							className="inline-flex items-center px-4 py-2 border border-gray-300 text-base font-semibold rounded-md text-indigo-600 bg-white hover:bg-gray-50 drop-shadow-md"
+						>
+							Recipes
+						</button>
+						<button
+							onClick={() => {
+								changeToRestaurants();
+							}}
+							className="inline-flex items-center ml-2 px-4 py-2 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 drop-shadow-md"
+						>
+							Restaurants
+						</button>
+					</div>
+					<div className="mx-6 my-6 grid grid-cols-1 gap-3">
+						{liked_restaurants}
+					</div>
+				</div>
+			);
+		}
+	}
 }
 
 Likes.getInitialProps = async (ctx) => {
-  var recipes = JSON.parse(window.sessionStorage.getItem("recipes"));
+	var recipes = JSON.parse(window.sessionStorage.getItem("recipes"));
+	var restaurants = JSON.parse(window.sessionStorage.getItem("restaurants"));
 
-  if (!recipes) {
-    recipes = [];
-  }
+	if (!recipes) {
+		recipes = [];
+	}
 
-  return { LikedRecipes: recipes };
+	if (!restaurants) {
+		restaurants = [];
+	}
+
+	return { LikedRecipes: recipes, LikedRestaurants: restaurants };
 };
